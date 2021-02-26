7ca279b08d119d069c820571be2cff85fbef2f9f
Nivratti <boyanenivratti@gmail.com>
2021-02-26 09:23:32 +0530

    added api route to demonstrate how to get url params

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 
 #include "crow.h"
 
+
 int main()
 {
     crow::SimpleApp app;
@@ -16,6 +17,23 @@ int main()
     CROW_ROUTE(app, "/")([](){
         return "Hello world";
     });
+
+    // handling params
+    CROW_ROUTE(app, "/params")
+    ([](const crow::request& req){
+        // // std::cout<<req.body;
+        // std::cout<< req.url_params;
+        // std::cout<< req.url_params.get("a");
+
+        std::ostringstream os;
+        os << "Params: " << req.url_params << "\n\n"; 
+        os << "The key 'foo' was " << (req.url_params.get("foo") == nullptr ? "not " : "") << "found.\n";
+        if(req.url_params.get("pew") != nullptr) {
+            double countD = boost::lexical_cast<double>(req.url_params.get("pew"));
+            os << "The value of 'pew' is " <<  countD << '\n';
+        }
+        return crow::response{os.str()};
+    });    
 
     app.server_name("CrowCpp").port(18080).multithreaded().run();
 }
